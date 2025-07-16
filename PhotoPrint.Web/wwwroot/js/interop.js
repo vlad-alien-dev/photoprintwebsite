568ad094abcfa720d78f893f831fca10c680c126
v.budnikov <v.budnikov@NB-1B6LQU>
2025-07-16 18:44:05 +0300

    Optimized images rendering speed

---
+++ PhotoPrint.Web/wwwroot/js/interop.js
@@ -0,0 +1,8 @@
+﻿export function createObjectURLFromInput(inputId) {
+    const input = document.getElementById(inputId);
+    if (!input || !input.files || input.files.length === 0) {
+        throw new Error("File input not found or no file selected");
+    }
+    const file = input.files[0];
+    return URL.createObjectURL(file);
+}
